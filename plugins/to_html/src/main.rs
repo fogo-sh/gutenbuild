db73f2ea0dd333c507d81c87fff06225617788f9
Mitchell Hynes <me@mitchellhynes.com>
2021-07-03 10:20:59 -0230

    Progress on to_html

--- plugins/to_html/src/main.rs
+++ plugins/to_html/src/main.rs
@@ -1,38 +1,55 @@
+use anyhow::Result;
 use pulldown_cmark::{html, Options, Parser};
 use std::fs;
-use std::path::Path;
+use std::path::PathBuf;
 
-fn process(current_dir: &str) -> Result<(), String> {
-    for entry in fs::read_dir(current_dir).map_err(|err| format!("{}", err))? {
-        let entry = entry.map_err(|err| format!("{}", err))?;
-        let path = entry.path();
-        if let Some(extension) = &path.extension() {
-            let contents =
-                fs::read_to_string(&path).expect("Something went wrong reading the file");
-            if extension.to_str().unwrap() == "md" {
-                let mut options = Options::empty();
-                options.insert(Options::ENABLE_STRIKETHROUGH);
-                let parser = Parser::new_ext(&contents, options);
-                let mut html_output = String::new();
-                html::push_html(&mut html_output, parser);
-                fs::write(
-                    Path::new(&format!(
-                        "{}.html",
-                        &path
-                            .file_stem()
-                            .expect("Should have a file name")
-                            .to_str()
-                            .unwrap()
-                    )),
-                    html_output,
-                )
-                .expect("Couldn't write file");
+struct ReadFile {
+    path: PathBuf,
+    contents: String,
+}
+
+impl ReadFile {
+    fn write(&self) -> Result<()> {
+        fs::write(&self.path, &self.contents)?;
+        Ok(())
+    }
+}
+
+fn read_all(current_dir: &str) -> Result<Vec<ReadFile>> {
+    fs::read_dir(current_dir)?
+        .filter_map(|entry| {
+            // TODO: handle ok as return error result
+            let entry = entry.ok()?;
+            let path = entry.path();
+            if let Some(extension) = &path.extension() {
+                if extension.to_str().unwrap() == "md" {
+                    let contents = fs::read_to_string(&path).ok()?;
+                    return Some(Ok(ReadFile { path, contents }));
+                }
             }
-        }
+            None
+        })
+        .collect()
+}
+
+fn to_html(files: &mut Vec<ReadFile>) -> Result<()> {
+    for mut file in files {
+        let mut options = Options::empty();
+        options.insert(Options::ENABLE_STRIKETHROUGH);
+        let parser = Parser::new_ext(&file.contents, options);
+        let mut html_output = String::new();
+        html::push_html(&mut html_output, parser);
+        file.path.set_extension("html");
+        file.contents = html_output;
     }
     Ok(())
 }
 
-fn main() -> Result<(), String> {
-    process("./")
+fn main() -> Result<()> {
+    let mut read_files = read_all("./")?;
+    to_html(&mut read_files)?;
+    for file in read_files {
+        file.write()?;
+    }
+    Ok(())
 }
---
+++ posts/test.md
@@ -0,0 +1,11 @@
+---
+title = "asdf"
+tags = ["hello", "world"]
+slug = "hello"
+created_date = 2-7-2021
+last_created_date = 2-7-2021
+---
+
+# h1
+
+## h2
